995467c052c75cc9d099707e901dbe820aa872ba
Carloto11 <yasmincarlotobezerra17@gmail.com>
2023-03-31 13:57:51 -0300

    modificando o banco e o titular

--- AppleBank/AppleBank/titular/titular.swift
+++ AppleBank/AppleBank/titular/titular.swift
@@ -2,10 +2,10 @@ import Foundation
 
 public class Titular {
     var nome: String
-    private var CPF: Int
+    private var CPF: String
     private var dataDeNascimento: String
     
-    init(nome: String, CPF: Int, dataDeNascimento: String){
+    init(CPF: String, nome: String, dataDeNascimento: String){
         self.nome = nome
         self.CPF = CPF
         self.dataDeNascimento = dataDeNascimento
